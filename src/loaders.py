6f58abe8cfb580d671a3433b7fa2b66293778229
Michu M <michal.mmtk@gmail.com>
2022-06-03 21:09:53 +0200

    WIP

--- src/loaders.py
+++ src/loaders.py
@@ -6,12 +6,11 @@ def load_dataset(path, sep):
 
 def load_breast_tissue():
     # 106 instances
-    # 10 attributes: 9 features + 1 class attribute
-
+    # 10 attributes: 9 features + 1 class attribute under "Class" column
     df = pandas.read_csv("../data/extracted/breast_tissue.csv", delimiter=";")
-    df.drop('Case #', axis='columns', inplace=True)
-    #klasa pod class
-    return df
+    x = df.drop(['Case #', 'Class'], axis='columns')
+    y = df['Class']
+    return x, y
 
 
 def load_car_evaluation():
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from loaders import *
 from model.model import Model
-
+from sklearn import tree
+import numpy as np
+import pandas as pd
 
 def calculate_for_all_combinations(parameters):
     combinations = get_dictionary_combinations(parameters)
@@ -41,15 +43,40 @@ def get_all_combinations(parameters_lists):
 
 if __name__ == "__main__":
     # d1 = load_dataset("../data/extracted/breast_tissue.csv", ";")
-    d1 = load_breast_tissue()
-    d2 = load_car_evaluation()
-    d3 = load_orders_data()
-    d4 = load_titanic()
-    d5 = load_wine()
-    d6 = load_wine_quality_red()
+    X, Y = load_breast_tissue()
+    # d2 = load_car_evaluation()
+    # d3 = load_orders_data()
+    # d4 = load_titanic()
+    # d5 = load_wine()
+    # d6 = load_wine_quality_red()
     # print(d6)
-    sth = Model({"mut_prob": 0.5})
-    # sth.calculate()
-    parameters = {"mut_prob": [0.5, 0.1], "cross_prob": [0.3, 0.2]}
-    calculate_for_all_combinations(parameters)
+    # sth = Model({"mut_prob": 0.5})
+    # # sth.calculate()
+    # parameters = {"mut_prob": [0.5, 0.1], "cross_prob": [0.3, 0.2]}
+    # calculate_for_all_combinations(parameters)
+
+    test_ratio = 0.3
+    train_indices = np.random.rand(len(X)) > test_ratio
+    X_train = X.loc[train_indices]
+    X_test = X.loc[~train_indices]
+    Y_train = Y.loc[train_indices]
+    Y_test = Y.loc[~train_indices]
+
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X_train, Y_train)
+    print(clf.predict(X_test))
+    print(Y_test)
+    preds = clf.predict(X_test)
+    # print(np.sum(preds == Y_test)/len(Y_test))
+    # outcome = Y_test.insert(1, "predictions", pd.Series(preds))
+    outcome = pd.concat([Y_test, pd.DataFrame(preds, index=Y_test.index)], axis=1, ignore_index=True)
+    outcome.columns = ["class", "prediction"]
+    # # outcome = Y_test.append(pd.Series(preds), ignore_index=True, axis=1)
+    print(outcome)
+    outcome.to_csv('breast_tissue.csv')
+
+    print(pd.read_csv('breast_tissue.csv', index_col=0))
+
+
+
 
