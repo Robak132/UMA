effaca7686fca7d388ee7d07e64db92456b7c383
Jakub Robaczewski <kubarob13@gmail.com>
2022-06-06 15:16:36 +0200

    Michał mi kazał przestać

--- src/model/decision_tree.py
+++ src/model/decision_tree.py
@@ -1,12 +1,23 @@
 from sklearn import tree
 
+from utils import wrap_labels_with_predictions_to_dataframe, calculate_accuracy
+
 
 class DecisionTreeClassifier:
     def __init__(self):
         self.clf = tree.DecisionTreeClassifier()
 
-    def train(self, x, y):
-        self.clf.fit(x, y)
+    def train(self, dataset):
+        self.clf.fit(dataset.x_train, dataset.y_train)
 
-    def predict(self, x):
-        return self.clf.predict(x)  # TODO sprawdzić czy to się tak ładnie zwróci po kolei
+    def predict(self, dataset):
+        return self.clf.predict(dataset.x_test)  # TODO sprawdzić czy to się tak ładnie zwróci po kolei
+
+    def calc_accuracy(self, dataset):
+        predictions = self.predict(dataset)
+        labels = dataset.y_test
+
+        outcome = wrap_labels_with_predictions_to_dataframe(labels, predictions)
+        accuracy = calculate_accuracy(predictions, labels)
+
+        return accuracy, outcome
